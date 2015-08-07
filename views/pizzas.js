0418492f2387505c10cb143364cf2b1eb5ab977d
Robert Lemon <rob.lemon@gmail.com>
2015-08-07 10:37:19 -0400

    makes pizzas unique

--- views/pizzas.js
+++ views/pizzas.js
@@ -5,7 +5,7 @@
 	var table = "<table class='bordered striped hoverable centered'>"
 	for(var i = 0; i < res.values.length; i++){
 		var value = res.values[i];
-		table += "<tr><td><img src='http://lorempizza.com/100/100?breaker="+(new Date())+ "' /></td><td>" + value[0] + "</td><td>" + value[1] + "</td><td>" + value[2] + "</td></tr>"
+		table += "<tr><td><img src='http://lorempizza.com/100/100?breaker="+(Math.random())+ "' /></td><td>" + value[0] + "</td><td>" + value[1] + "</td><td>" + value[2] + "</td></tr>"
 	}
 	table += "</table>";
 	pizzas.innerHTML = table;
